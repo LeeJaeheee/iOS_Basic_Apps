30763d82438a090cafb40d374145e6dfcf334c1f
LeeJaeheee <sns3434@gmail.com>
2023-06-21 01:45:11 +0900

    ChatListCollectionViewCell 컨텐츠 삽입, ChatListViewController 업데이트

---
+++ 01_List_and_Grid/02_ChatList/ChatList/ChatListCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ChatListCollectionViewCell.swift
+//  ChatList
+//
+//  Created by 이재희 on 2023/06/20.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var chatLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    func configure(_ chat: Chat) {
+        thumbnail.image = UIImage(named: chat.name)
+        nameLabel.text = chat.name
+        chatLabel.text = chat.chat
+        dateLabel.text = formattedDateString(dateString: chat.date)
+    }
+    
+    func formattedDateString(dateString: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        if let date = formatter.date(from: dateString) {
+            formatter.dateFormat = "M/d"
+            return formatter.string(from: date)
+        } else {
+            return ""
+        }
+    }
+}
