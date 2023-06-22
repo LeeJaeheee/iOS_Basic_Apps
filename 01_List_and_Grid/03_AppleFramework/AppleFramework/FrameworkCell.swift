dcfd7efb1624aa853a351b874d2a9a0d2eb45afa
LeeJaeheee <sns3434@gmail.com>
2023-06-22 12:51:58 +0900

    FrameworkCell 클래스 생성, ViewController 내용 추가

---
+++ 01_List_and_Grid/03_AppleFramework/AppleFramework/FrameworkCell.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkCell.swift
+//  AppleFramework
+//
+//  Created by 이재희 on 2023/06/22.
+//
+
+import UIKit
+
+class FrameworkCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    
+    func configure(_ framework: AppleFramework) {
+        thumbnailImageView.image = UIImage(named: framework.imageName)
+        nameLable.text = framework.name
+    }
+    
+}
--- 01_List_and_Grid/03_AppleFramework/AppleFramework/FrameworkListViewController.swift
+++ 01_List_and_Grid/03_AppleFramework/AppleFramework/FrameworkListViewController.swift
@@ -9,21 +9,60 @@ import UIKit
 
 class FrameworkListViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let list: [AppleFramework] = AppleFramework.list
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
+    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension FrameworkListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return list.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
+            return UICollectionViewCell()
+        }
+        
+        let framework = list[indexPath.item]
+        cell.configure(framework)
+        return cell
+    }
+    
+}
 
+extension FrameworkListViewController: UICollectionViewDelegateFlowLayout {
+    
+    // CollectionView의 Estimate size : Automatic -> None으로 변경 -> 사이즈는 아래 함수만 참고하도록 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // 셀 사이 수평 간격
+        let interItemSpacing: CGFloat = 10
+        
+        // 셀의 너비와 높이 설정
+        let width = (collectionView.bounds.width - interItemSpacing * 2) / 3
+        let height = width * 1.5
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    // 셀 사이 수평 간격 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    // 라인 사이 수직 간격 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
 }
